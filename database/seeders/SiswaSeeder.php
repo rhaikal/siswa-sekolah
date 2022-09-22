83345814c596f5d16641284014ad78bf4a8dfb20
rhaikal <rhaikal101@gmail.com>
2022-09-22 19:58:31 +0700

    bug fix siswa relationship

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -25,18 +25,20 @@ class SiswaSeeder extends Seeder
             $tingkat_kelas =  rand(10, 12);
             for ($j=0; $j < 40; $j++) { 
                 $gender_siswa = $jenis_kelamin[rand(0, 1)];
-                $siswa_id = fake()->uuid();
                 $nama_siswa = fake()->name(($gender_siswa == 'laki-laki') ? 'male' : 'female');
-                DB::collection('siswa')->insert([
-                    '_id' => $siswa_id,
+                
+                $siswa_id = DB::collection('siswa')->insertGetId([
                     'nis' => fake()->nik(),
                     'nama' => $nama_siswa,
                     'kelas_id' => $kelas->_id,
-                    'kelas' => [$tingkat_kelas, $jurusan_kelas],
+                    'kelas' => [
+                        'tingkat' => $tingkat_kelas, 
+                        'jurusan' => $jurusan_kelas
+                    ],
                     'jenis_kelamin' => $gender_siswa,
                     'agama' => $agama[rand(0, 5)],
                     'alamat' => fake()->address(),
-                    'foto' => fake()->imageUrl(360, 360, 'person', true, 'siswa', true, 'jpg')
+                    'foto' => fake()->imageUrl(360, 360, 'person', true, 'siswa', true, 'jpg'),
                 ]);
                 
                 DB::collection('kelas')->where('_id', $kelas->_id)->push('siswa', ["_id" => $siswa_id, 'nama' => $nama_siswa]);
